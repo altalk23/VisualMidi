6896bba2e620d7f90d080a099bf8943df20e8ec4
altalk23 <altalk.23@robcol.k12.tr>
2020-01-10 23:16:49 +0300

    and fixed

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from mido import MidiFile
 # Midi
 trackcount, maxnote = 3, 8
 data = np.zeros((trackcount, maxnote, 4), dtype=np.uint32)
-idx = [0,0,0]
-time = 0
 cont = np.full((128), -1, dtype=np.int32)
 
 mid = MidiFile('logic.mid')
@@ -14,23 +12,24 @@ for trackidx, track in enumerate(mid.tracks):
     print('Track {}: {}'.format(trackidx, track.name))
     # using note on note off for now
     time = 0
+    idx = 0
     for msg in track:
         if not msg.is_meta: print(msg)
         print('')
         time += msg.time
 
         if msg.type == 'note_on':
-            data[trackidx][idx[trackidx]] = [time, msg.note, msg.velocity, 0]
-            cont[msg.note] = trackidx
-            idx[trackidx] += 1
+            data[trackidx][idx] = [time, msg.note, msg.velocity, 0]
+            cont[msg.note] = idx
+            idx += 1
             pass
 
         elif msg.type == 'note_off':
             data[trackidx][cont[msg.note]][3] = time
             cont[msg.note] = -1
             pass
+print(data[1:])
 
-print(data)
 
 # Image
 '''
