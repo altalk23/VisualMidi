b0d6b68e1ed02ff671a4e9557d8314c2344bcf74
altalk23 <altalk.23@robcol.k12.tr>
2020-01-11 17:06:35 +0300

    key coordinates

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ parser.add_argument('-wi', '--width', type=int, default=1920, help='width of the
 parser.add_argument('-hi', '--height', type=int, default=1080, help='height of the video')
 parser.add_argument('-s', '--start', type=int, default=21, help='start note')
 parser.add_argument('-e', '--end', type=int, default=108, help='end note')
+parser.add_argument('-st', '--stretch', type=int, default=960000000, help='stretch constant')
 
 
 args = parser.parse_args()
@@ -18,9 +19,10 @@ args = parser.parse_args()
 mid = MidiFile(args.midifile)
 width, height = args.width, args.height
 start, end = args.start, args.end+1
+stretch = args.stretch
 
 # Midi
-'''
+
 trackcount, maxnote = 2, 8
 data = np.zeros((trackcount, maxnote, 4), dtype=np.uint64)
 cont = np.full((128), -1, dtype=np.int32)
@@ -77,7 +79,44 @@ for trackidx, track in enumerate(mid.tracks[1:]):
     print('')
 print(data)
 print(tempo)
-'''
+
+
+
+
+
+# Keyboard visual
+octave = np.array([1,0,1,0,1,1,0,1,0,1,0,1])
+full = np.tile(octave, 11)[:128]
+keyboard = full[start:end]
+whitecount = np.count_nonzero(keyboard == 1)
+notes = np.zeros((end-start,2), dtype=np.uint16)
+idx = 0
+for note in range(end-start):
+    if keyboard[note] == 1: idx += 1
+    whitewidth = round((width * idx) / whitecount)
+    blackwidth = whitewidth / 1.5
+    if keyboard[note] == 1:
+        if (keyboard[note-1]) == 1:
+            notes[note] = [notes[note-1][1], whitewidth]
+        else:
+            notes[note] = [notes[note-2][1], whitewidth]
+    else:
+        if (start+note)%12 == 1:
+            notes[note] = [round(notes[note-1][1]-blackwidth/1.5), round(notes[note-1][1]+blackwidth/3)]
+            pass
+        elif (start+note)%12 == 3:
+            notes[note] = [round(notes[note-1][1]-blackwidth/3), round(notes[note-1][1]+blackwidth/1.5)]
+            pass
+        elif (start+note)%12 == 6:
+            notes[note] = [round(notes[note-1][1]-blackwidth*0.75), round(notes[note-1][1]+blackwidth*0.25)]
+            pass
+        elif (start+note)%12 == 8:
+            notes[note] = [round(notes[note-1][1]-blackwidth*0.5), round(notes[note-1][1]+blackwidth*0.5)]
+            pass
+        elif (start+note)%12 == 10:
+            notes[note] = [round(notes[note-1][1]-blackwidth*0.25), round(notes[note-1][1]+blackwidth*0.75)]
+            pass
+print(notes)
 
 
 # Image
@@ -87,10 +126,3 @@ data[0:256, 0:256] = [255, 127, 0] # red patch in upper left
 img = Image.fromarray(data, 'RGB')
 img.save('my.png')
 '''
-
-# Keyboard visual
-octave = np.array([1,0,1,0,1,1,0,1,0,1,0,1])
-full = np.tile(octave, 11)[:128]
-keyboard = full[start:end]
-whitecount = np.count_nonzero(keyboard == 1)
-print(whitecount)
